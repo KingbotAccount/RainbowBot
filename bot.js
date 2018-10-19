28ddcaba52f163576a0a5ac5747dacad6d8794de
KingbotAccount <44236472+KingbotAccount@users.noreply.github.com>
2018-10-19 12:39:47 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+iconst suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
+const prefix = "r#";
+
+client.on("message", message => {
+    fs.writeFile('./suck.json', JSON.stringify(suck));
+});
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.forEach(g => {
+            if (suck[g.id]) {
+                if (suck[g.id].role) {
+                    var role = g.roles.get(suck[g.id].role);
+                    if (role) {
+                        role.edit({color : "RANDOM"});
+                    };
+                };
+            };
+        });
+    }, 1500);
+})
+
+client.on("message", message => {
+    if (!message.content.startsWith(prefix)) return;
+    if (message.author.bot) return;
+    if (message.channel.type !== "text") return message.reply("This Command Is Only Allowed In Servers");
+    var args = message.content.split(" ");
+    var command = args[0].slice(prefix.length);
+    switch(command) {
+        case "r#set" :
+        if (message.member.hasPermission("MANAGE_ROLES")) return message.reply("no no");
+        message.guild.createRole({name : "rainbow", color : "RANDOM"}).then(r => {
+            r.edit({color : "RANDOm"});
+            suck[message.guild.id] = {role : r.id};
+        });
+    };
+});
+client.on("message", message => {
+  if (message.content === "d!help") {
+message.author.send(`**
+r#set 
+ - لإنشاء رتبة الرينبو وبدا الرينبو
+- To create the role of the Rainbow & Start The Rainbow
+r#inv 
+- لدعوة البوت
+- To Invite the bot
+
+خطوات لو الرتبة م أشتغلت .!!
+1- ضع رتبة الرينبو فوق الالوان أو الرتب الملونه لو فيه
+2- ضع رتبة البوت فوق رتبة الرينبو
+The steps of the role did not worked .!!
+1- Place the role of the Rainbow above the colors or colored ranks if it
+2- Put the bot role above the role of the Rainbow 
+رآبط البوت - Bot Invite link
+- http://cutt.us/RainbowBott
+**`)
+
+client.login(process.env.BOT_TOKEN)
